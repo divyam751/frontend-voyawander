5a36b975b303e4ecc0eb9aa85165a1d0e51c96c4
Divyam Chauhan <125983433+divyam751@users.noreply.github.com>
2024-03-02 09:56:47 +0530

    Image updated

--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -17,7 +17,7 @@ const Hotels = () => {
       </div>
       <div>
         <img
-          src='https://inato.com/static/094c124358273399e0abb5a636383989/0d4fb/inato-world.webp'
+          src='https://private-user-images.githubusercontent.com/125983433/309440418-c1e43285-3bed-4a12-880f-e0b827d5f068.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDkzNTM4NTUsIm5iZiI6MTcwOTM1MzU1NSwicGF0aCI6Ii8xMjU5ODM0MzMvMzA5NDQwNDE4LWMxZTQzMjg1LTNiZWQtNGExMi04ODBmLWUwYjgyN2Q1ZjA2OC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMzAyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDMwMlQwNDI1NTVaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1mOGQxNDI5NDdmZDZhYzE0ZDFkODEyNTg4YTEwNDg2YmE4MTg1ZjM3NzUxNTk4Y2YyYzU2Y2ZlODQ2N2FlMTkzJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.-a6fowgOTFOkUlYKz_3OVGITbKxNP1-2DdPXKqb8fU4'
           alt='map'
         />
       </div>
